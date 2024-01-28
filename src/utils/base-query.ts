f5ea0732e37f0fb4a6048f50cfcfc6f09999a2c4
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2024-01-28 01:48:34 +0100

    feat(utils): add base-query and utils modules

---
+++ src/utils/base-query.ts
@@ -0,0 +1,65 @@
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+
+export type BaseQueryFnType = <
+  ResultType,
+  ErrorType,
+  QueryFnType extends (...args: any[]) => any
+>(
+  queryFn: QueryFnType
+) => Promise<{ data?: ResultType; error?: ErrorType }>;
+
+/**
+ * A base query function for `react-query` that uses `axios` to make requests.
+ * @param baseUrl The base URL to use for all requests.
+ * @returns A base query function for `react-query`.
+ *
+ * @example
+ * ```ts
+ * import { useQuery } from 'react-query';
+ * import { axiosBaseQuery } from './blackAxios';
+ *
+ * const baseQueryFn = axiosBaseQuery();
+ *
+ * function useUserQuery() {
+ *  return useQuery('user', () => baseQueryFn({ url: '/user' }));
+ * }
+ */
+export const axiosBaseQuery = <ResultType, ErrorType>(
+  baseUrl: string,
+  inject: (config: AxiosRequestConfig) => AxiosRequestConfig = (config) =>
+    config
+): BaseQueryFnType =>
+  async function axiosBaseQuery<
+    ResultType,
+    ErrorType,
+    QueryFnType extends AxiosRequestConfig & { body?: any; token?: string }
+  >(queryFn: QueryFnType): Promise<{ data?: ResultType; error?: ErrorType }> {
+    try {
+      const {
+        url,
+        method,
+        data,
+        body,
+        params,
+        headers,
+        token: __,
+      } = queryFn as AxiosRequestConfig & { body?: any; token?: string };
+
+      const result = await axios({
+        url: baseUrl + url,
+        method,
+        data: body || data,
+        params,
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers,
+        },
+        ...inject(queryFn),
+      });
+
+      return result.data;
+    } catch (axiosError) {
+      const err = axiosError as AxiosError<ErrorType>;
+      throw err?.response?.data || err;
+    }
+  };
